f0d1380660df00e0bba3abfd053141a0a82c3f0f
sahijdeepsingh <sahijdeepsingh817@gmail.com>
2024-08-11 01:56:11 +0530

    js file added

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-
-// This Function to handle search form submission. 
-document.getElementById('search-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
-    const query = document.getElementById('search-input').value;
-    alert('Search for: ' + query);
-  });
-  
-  // This function is log messages when a link is clicked. 
-  document.querySelectorAll('.nav-link, .aside-link').forEach(link => {
-    link.addEventListener('click', function(event) {
-      console.log('Link clicked: ' + this.textContent);
-    });
-  });
-    
+// java script will be added here
